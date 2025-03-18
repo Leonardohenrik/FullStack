878ca1b165af8e946b650920d3fcb37ac1b11c6b
Leonardohenrik <leonardoface_519@hotmail.com>
2025-03-17 22:30:57 -0300

    adição aula 5

---
+++ Aula 5/script.js
@@ -0,0 +1,28 @@
+//window.alert("olá");
+//console.log("olá");
+//console.log("hello world");
+
+
+//let nome = prompt("Qual o seu nome?")
+//console.log(nome);
+//let i = 0;
+//while(i < 10){
+//    console.log(i)
+//    i++;
+
+//}
+
+
+
+//for(let i = 0; i < 10; i++){
+//console.log(i)
+//}
+
+//for(let i = 1; i < 100; i+=2){
+   // console.log(i)
+//}
+
+//let i = 5;
+//while(i <= 500){
+  //  console.log(i)
+//}
