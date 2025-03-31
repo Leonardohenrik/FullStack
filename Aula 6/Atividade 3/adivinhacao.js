5ed5ca2f2a6ac24e20107c9121e7d688baffc69d
Leonardohenrik <leonardoface_519@hotmail.com>
2025-03-31 14:17:58 -0300

    aula 6

--- Aula 6/Atividade 3/adivinhacao.js
+++ Aula 6/Atividade 3/adivinhacao.js
@@ -1,3 +1,20 @@
-function Math.random(){
+const Random = Math.floor(Math.random() * 100);
+console.log("Número aleatório gerado: ", Random);
+
+
+function guess() {
     
+    const palpite = parseInt(document.getElementById("palpite").value);
+
+
+    if (palpite === Random) {
+        document.getElementById("div_3").textContent = "Parabéns, você acertou!";
+        document.getElementById("div_3").style.color = "green";
+    } else if (palpite < Random) {
+        document.getElementById("div_3").textContent = "O número é maior. Tente novamente!";
+        document.getElementById("div_3").style.color = "red";
+    } else {
+        document.getElementById("div_3").textContent = "O número é menor. Tente novamente!";
+        document.getElementById("div_3").style.color = "red";
+    }
 }
