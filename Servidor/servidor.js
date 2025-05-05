01310db3facdcc6e04a48a300ef4418279d2ac25
leonardohenrik <leonardoface_519@hotmail.com>
2025-05-05 20:55:00 -0300

    1

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -11,6 +11,20 @@ app.use(express.static("./public"));
 
 var server = http.createServer(app);
 
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+const uri = "mongodb+srv://leohenrik:<1d1i2b2i>@henriks.ct5jrkx.mongodb.net/?retryWrites=true&w=majority&appName=Henriks"
+
+const client = new MongoClient(uri, {useNewUrlParser: true});
+
+var dbo = client.db("exemplo_bd");
+
+var usuarios = dbo.collection("usuarios");
+
+
+
 server.listen(80);
 
 
